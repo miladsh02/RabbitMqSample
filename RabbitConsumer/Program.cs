b8b0a2123d95663b0e6d718f2eef011e6d543805
Milad <milad.shafiei81@gmail.com>
2023-07-19 16:27:45 +0330

    feat: add consumer but it has a bug

--- RabbitConsumer/Program.cs
+++ RabbitConsumer/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory=new ConnectionFactory()
+            {
+                 HostName="localhost",
+                 UserName="admin",
+                 Password="admin",
+                 VirtualHost="/"
+            };
+
+            var connection=factory.CreateConnection();
+
+            using var channel=connection.CreateModel();
+            channel.QueueDeclare("MessageQueue",durable:true,exclusive:false);
+            var consumer=new EventingBasicConsumer(channel);
+            consumer.Received+=(model,eventArgs)=>
+            {
+                //binnary message
+                var body=eventArgs.Body.ToArray();
+                var message=Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+Console.ReadKey();
