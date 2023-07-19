711aaed9c1caec8a6661bfd0555214989ef7fdee
Milad <milad.shafiei81@gmail.com>
2023-07-19 14:45:29 +0330

    feat: add a message controller to add message to queue

---
+++ WebApi/Controllers/MessageController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using RabbitMqSample.Models;
+using WebApi.Services;
+
+namespace RabbitMqSample.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MessageController : ControllerBase
+{
+    private readonly IMessageProducer _messageProducer;
+    public static readonly List<MessageModel> _messages=new List<MessageModel>();
+    public MessageController(IMessageProducer messageProducer)
+    {
+        _messageProducer=messageProducer;
+    }
+
+    [HttpPost]
+    public IActionResult SendMessageToQueue(MessageModel message)
+    {
+        if(message is null)
+        return BadRequest("The message is null");
+
+        _messages.Add(message);
+        
+        _messageProducer.Publish(message);
+
+        return Ok();
+    }
+}
