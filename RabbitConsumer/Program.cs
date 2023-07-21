15a6495affae29a107a0dc3743c4ad64d6c89589
Milad <milad.shafiei81@gmail.com>
2023-07-21 18:13:11 +0330

    feat:Design consumer in console app

--- RabbitConsumer/Program.cs
+++ RabbitConsumer/Program.cs
@@ -1,32 +1,24 @@
 ﻿
 using System.Text;
-using System.Text.Json;
 using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
 
-        var factory = new ConnectionFactory
-        {
-            HostName="localhost",
-            UserName="admin",
-            Password="admin",
-            VirtualHost="/"
-        };
+var factory=new ConnectionFactory()
+{
+        HostName="localhost",
+        UserName="admin",
+        Password="admin",
+        VirtualHost="/"
+};
 
-        using var connection = factory.CreateConnection();
-        using var channel = connection.CreateModel();
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("MessageQueue",durable:true,exclusive: false);
 
-        channel.QueueDeclare("MessageQueue", durable: true, exclusive: false);
-
-        var consumer = new EventingBasicConsumer(channel);
-        consumer.Received += (model, eventArgs) =>
-        {
-            var body = eventArgs.Body.ToArray();
-            var message = Encoding.UTF8.GetString(body);
-            Console.WriteLine(message);
-        };
-
-        //channel.BasicConsume(queue: "MessageQueue", autoAck: true, consumer: consumer);
-
-        Console.WriteLine("Press any key to exit.");
-        connection.Close();
-    
+var data = channel.BasicGet("MessageQueue", autoAck: true);
+if (data != null)
+{
+    var message = Encoding.UTF8.GetString(data.Body.ToArray());
+    Console.WriteLine(message);
+}
+connection.Close();
+Console.WriteLine("press a button to exit");  
